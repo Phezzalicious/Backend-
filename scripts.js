fc2a5542ee6354dd7c3de6485849772eaed25667
Phelps Merrell <starfish206@gmail.com>
2019-09-04 14:02:19 -0500

    third commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function takeInput(){
+    var color = document.getelementbyid("colors").value;
+    var emotion = document.getelementbyid("emotions").value;
+    var fName = document.getelementbyid("fname").value;
+    var lName = document.getelementbyid("lname").value;
+    document.getElementById("name").innerHTML = color;
+  }
